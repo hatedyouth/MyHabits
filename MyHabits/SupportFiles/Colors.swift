e8f5816d400b2d7d6c777187a940afd1413e4683
hatedyouth <neverxforget@mail.ru>
2022-01-20 21:21:02 +0300

    add fonts and icons

---
+++ MyHabits/SupportFiles/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  MyHabits
+//
+//  Created by Тимур Турлыкин on 19.01.2022.
+//
+
+import UIKit
+
+struct ColorStyles {
+    static let purple: UIColor = UIColor(red: 161 / 255.0 , green: 22 / 255.0 , blue: 204 / 255.0 , alpha: 1)
+   
+}
